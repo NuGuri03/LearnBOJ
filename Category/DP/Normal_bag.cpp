9b5e9a1141caddf4c52322dafd6b8289db3e171c
NuGuri03 <sym2596@naver.com>
2024-12-16 16:30:22 +0900

    Optimization Normal_Bag

--- Category/DP/Normal_bag.cpp
+++ Category/DP/Normal_bag.cpp
@@ -7,17 +7,13 @@ typedef struct stuff {
     int w, v;
 }stuff;
 
-bool compare(const stuff &a, const stuff &b) {
-    return a.w < b.w;
-}
-
-int sol(vector<stuff> s, int n, int k) {
+int sol(vector<stuff> &s, int n, int k) {
     vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
     for (int i = 1; i <= n; i++) {
         for (int j = 0; j <= k; j++) {
-            dp[i][j] = max(dp[i - 1][j], dp[i][j]);
+            dp[i][j] = max({dp[i - 1][j], dp[i][j], dp[i][j - 1]});
             if (j + s[i - 1].w <= k) {
-                dp[i][j + s[i - 1].w] = max(dp[i][j], dp[i - 1][j] + s[i - 1].v);
+                dp[i][j + s[i - 1].w] = dp[i - 1][j] + s[i - 1].v;
             }
         }
     }
@@ -35,7 +31,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> s[i].w >> s[i].v;
     }
-    sort(s.begin(), s.end(), compare);
     
     cout << sol(s, n, k) << '\n';
 }
